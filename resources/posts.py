5b3e11456fac020121b1fdf143c285653d1a557f
Sarah <sarah.alhuriz@hotmail.com>
2021-01-20 16:34:37 -0700

    add update and delete routes

--- resources/posts.py
+++ resources/posts.py
@@ -25,7 +25,24 @@ def create_post():
 
 
 @posts.route('/<id>', methods=["GET"])
-def get_one_post(id)
-post = models.Posts.get_by_id(id)
-print(post.__dict__)
-return jsonify(data=model_to_dict(post), status={"code": 200, "message": "success"})
+def get_one_post(id):
+  post = models.Posts.get_by_id(id)
+  print(post.__dict__)
+  return jsonify(data=model_to_dict(post), status={"code": 200, "message": "success"})
+
+
+@posts.route('/<id>', methods=["PUT"])
+def update_post(id):
+  payload = request.get_json()
+  query = models.Posts.update(**payload).where(models.Posts.id == id)
+  query.execute()
+  return jsonify(data=model_to_dict(models.Posts.get_by_id(id)), status={"code": 200, "message": "success"})
+
+@posts.route('/<id>', methods= ["DELETE"])
+def delete_post(id):
+  post = models.Posts.get_by_id(id)
+  post_dict = model_to_dict(post)
+  print(post_dict)
+  query = models.Posts.delete().where(models.Posts.id == id)
+  query.execute()
+  return jsonify(data=post_dict, status={"code": 200, "message": "success, deleted"})
